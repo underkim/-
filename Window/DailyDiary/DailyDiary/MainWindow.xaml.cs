2d34a904df6cb5c98ecac8a430afe5872b12e30d
underkim <rlaehdgns4568@gmail.com>
2024-11-03 14:50:08 +0900

    feat: 달력화면 추가

--- Window/DailyDiary/DailyDiary/MainWindow.xaml.cs
+++ Window/DailyDiary/DailyDiary/MainWindow.xaml.cs
@@ -20,5 +20,18 @@ namespace DailyDiary
         {
             InitializeComponent();
         }
+
+        private void Calendar_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // 사용자가 클릭한 날짜가 선택된 상태인지 확인
+            if (calendar.SelectedDate.HasValue)
+            {
+                DateTime selectedDate = calendar.SelectedDate.Value;
+
+                // 선택한 날짜로 일기 작성 창 열기
+                DiaryEntryWindow diaryEntryWindow = new DiaryEntryWindow(selectedDate);
+                diaryEntryWindow.ShowDialog();
+            }
+        }
     }
 }
