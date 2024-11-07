ad7091f807ec9a0939a758a5f5bd75c407a926fe
underkim <rlaehdgns4568@gmail.com>
2024-11-07 13:19:17 +0900

    feat: 기본 프로젝트 구조 설계

---
+++ Window/DailyDiary/DailyDiary/Model/DiaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DailyDiary.Model
+{
+    class DiaryEntry
+    {
+        string _id { get; set; }
+        string title { get; set; }
+        string content { get; set; }
+    }
+}
---
+++ Window/DailyDiary/DailyDiary/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyDiary.Model
+{
+    class User
+    {
+        public string email { get; set; }
+        public string googleToken { get; set; }
+    }
+}
